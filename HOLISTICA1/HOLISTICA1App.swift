913067c1ef73780d9a865ba92f31973094fd9eec
Miguel <ridruejomiguel@gmail.com>
2023-10-18 20:51:01 +0200

    Initial Commit

---
+++ HOLISTICA1/HOLISTICA1App.swift
@@ -0,0 +1,17 @@
+//
+//  HOLISTICA1App.swift
+//  HOLISTICA1
+//
+//  Created by Miguel Ridruejo on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HOLISTICA1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
